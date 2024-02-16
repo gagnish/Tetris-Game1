44e07f0a67fc544d2385577130f75823c1e6eb21
Gagnish Dhiman <gdhiman@SEA.CORP.EXPECN.COM>
2024-02-16 23:18:21 +0530

    adding new folder

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terrabucket22" # Replace with your actual S3 bucket name
     key    = "EKSterraform/terraform.tfstate"
-    region = "us-east-1a"
+    region = "us-east-1"
   }
 }
